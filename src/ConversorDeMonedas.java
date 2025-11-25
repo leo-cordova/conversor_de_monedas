c5809416e4bd17a5b54829bcc13b29fad4dae9c8
Leonel Cordova <necsarleonelvirgo@gmail.com>
2025-11-25 17:50:27 -0500

    se creo tres clases aplicando el principio de responsabilidad unica

---
+++ src/ConversorDeMonedas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConversorDeMonedas {
+    static void main() {
+        Scanner lectura = new Scanner(System.in);
+        String menu = """
+                Bienvenido al conversor de monedas
+                1. Convertir de Dólares a Euros
+                2. Convertir de Euros a Dólares
+                3. Convertir de Dólares a pesos mexicanos
+                4. Convertir de pesos mexicanos a Dólares
+                5. Convertir de Euros a pesos mexicanos
+                6. Convertir de pesos mexicanos a Euros
+                7. Salir
+                Por favor, seleccione una opción:
+                """;
+    }
+}
